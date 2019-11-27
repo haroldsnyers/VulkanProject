06d65ff323790d49589f9caaa852732e20f7d3b9
Snihar2 <16243@ecam.be>
2019-11-27 14:32:54 +0100

    adding rope

---
+++ VulkanRope/VulkanRope/shaders/compile.bat
@@ -0,0 +1,5 @@
+C:\VulkanSDK\1.1.121.2\Bin32/glslc.exe shader.vert -o vert.spv
+C:\VulkanSDK\1.1.121.2\Bin32/glslc.exe shader.frag -o frag.spv
+C:\VulkanSDK\1.1.121.2\Bin32/glslc.exe simple.vert -o simpleVert.spv
+C:\VulkanSDK\1.1.121.2\Bin32/glslc.exe simple.frag -o simpleFrag.spv
+pause
