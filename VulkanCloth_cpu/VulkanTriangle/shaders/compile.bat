7516406d90ef5e02bb25c29a96619ae32af51baa
Snihar2 <16243@ecam.be>
2020-01-14 04:11:06 +0100

    up cloth

---
+++ VulkanCloth_cpu/VulkanTriangle/shaders/compile.bat
@@ -0,0 +1,7 @@
+C:\VulkanSDK\1.1.121.2\Bin32/glslc.exe shader.vert -o vert.spv
+C:\VulkanSDK\1.1.121.2\Bin32/glslc.exe shader.frag -o frag.spv
+C:\VulkanSDK\1.1.121.2\Bin32/glslc.exe simple.vert -o simpleVert.spv
+C:\VulkanSDK\1.1.121.2\Bin32/glslc.exe simple.frag -o simpleFrag.spv
+C:\VulkanSDK\1.1.121.2\Bin32/glslc.exe shader.comp -o comp.spv
+
+pause
